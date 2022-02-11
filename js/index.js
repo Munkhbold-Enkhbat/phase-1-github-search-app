d956170e58cbbf5eac84ba0740f74ef97974f7ed
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-02-11 14:01:58 -0800

    Search for users

--- js/index.js
+++ js/index.js
@@ -2,17 +2,13 @@
   // const searchName = document.querySelector('#search')
   document.addEventListener('DOMContentLoaded', () => {
     getPersonYouWant()
-  })
-
-  
+  })  
 
   function getPersonYouWant() {
     document.querySelector('form').addEventListener('submit', (e) => {
       e.preventDefault()
       const searchName = e.target.search.value
-      fetch(`https://api.github.com/users/${searchName}`)
-      .then(res => res.json())
-      .then(personInfo => renderPerson(personInfo))
+      getAllUsers(searchName)
     })
   }
 
@@ -32,4 +28,10 @@ function renderPerson(person) {
     </p>
   </div>`
   document.querySelector('#user-list').appendChild(card)
+}
+
+function getAllUsers(name) {
+  fetch(`https://api.github.com/search/users?q=${name}`)
+  .then(res => res.json())
+  .then(data => data.items.forEach(person => renderPerson(person)))
 }
