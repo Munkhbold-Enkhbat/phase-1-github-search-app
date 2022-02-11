8f3845b30624b165dc411a21e1e7858a8361ebdd
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-02-11 12:09:58 -0800

    Updated person info card

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ function renderPerson(person) {
     <h2>${personName}</h2>
     <img class="person's image" src=${person.avatar_url}/>
     <p>
-      ${personName}'s profile link <a href='www.github.com'></a>
+      ${personName}'s profile link <a href='${person.html_url}'>${person.html_url}</a>
     </p>
   </div>`
   document.querySelector('#user-list').appendChild(card)
