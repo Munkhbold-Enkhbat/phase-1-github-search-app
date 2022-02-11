8a5e50a32dae82fcc06b321c95cd1b8fe03149bf
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-02-11 11:58:31 -0800

    Render person info

--- js/index.js
+++ js/index.js
@@ -1,11 +1,35 @@
 
   // const searchName = document.querySelector('#search')
-
-  document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault()
-    const searchName = e.target.search.value
-    fetch(`https://api.github.com/users/${searchName}`)
-    .then(res => res.json())
-    .then(data => console.log(data))
+  document.addEventListener('DOMContentLoaded', () => {
+    getPersonYouWant()
   })
 
+  
+
+  function getPersonYouWant() {
+    document.querySelector('form').addEventListener('submit', (e) => {
+      e.preventDefault()
+      const searchName = e.target.search.value
+      fetch(`https://api.github.com/users/${searchName}`)
+      .then(res => res.json())
+      .then(personInfo => renderPerson(personInfo))
+    })
+  }
+
+function renderPerson(person) {
+  const card = document.createElement('li')
+  const name = person.login
+  const personName = name[0].toUpperCase() + name.slice(1)
+
+  card.className = 'card'
+  card.style.textAlign = 'center'
+  card.innerHTML = `
+  <div>
+    <h2>${personName}</h2>
+    <img class="person's image" src=${person.avatar_url}/>
+    <p>
+      ${personName}'s profile link <a href='www.github.com'></a>
+    </p>
+  </div>`
+  document.querySelector('#user-list').appendChild(card)
+}
