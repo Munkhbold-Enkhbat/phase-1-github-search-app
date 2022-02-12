7c306368751bf371f650d308bea0b803d594c960
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-02-11 20:41:36 -0800

    Assignment Completed without Bonus part

--- js/index.js
+++ js/index.js
@@ -1,16 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const search = document.querySelector('#github-form')
+  const toggleBtn = document.createElement('button') 
+  toggleBtn.id = 'toggle-button' 
+  toggleBtn.innerText = 'user/repos'
+  
 
-  // const searchName = document.querySelector('#search')
-  document.addEventListener('DOMContentLoaded', () => {
-    getPersonYouWant()
-  })  
+  search.appendChild(toggleBtn)
+  search.addEventListener('submit', (e) => {
+    e.preventDefault()
+    clearPreviousResults()
+    const searchName = e.target.search.value
+    getAllUsers(searchName)
+  })
+})
 
-  function getPersonYouWant() {
-    document.querySelector('form').addEventListener('submit', (e) => {
-      e.preventDefault()
-      const searchName = e.target.search.value
-      getAllUsers(searchName)
-    })
-  }
+function clearPreviousResults() {
+  const resultList = document.querySelector('#user-list')
+  resultList.innerText = ''
+}
 
 function renderPerson(person) {
   const card = document.createElement('li')
@@ -22,16 +29,46 @@ function renderPerson(person) {
   card.innerHTML = `
   <div>
     <h2>${personName}</h2>
-    <img class="person's image" src=${person.avatar_url}/>
+    <img class='image' src=${person.avatar_url}/>
     <p>
       ${personName}'s profile link <a href='${person.html_url}'>${person.html_url}</a>
     </p>
+    <button class='repos-btn'>Repos</button>
   </div>`
+  card.querySelector('button').addEventListener('click', () => {
+    fetch(`https://api.github.com/users/${name}/repos`)
+    .then(res => res.json())
+    .then(repos => {
+      clearPreviousRepos()
+      addTitle(personName)
+      repos.forEach((repo, index) => renderRepos(repo, index))
+    })
+  })
   document.querySelector('#user-list').appendChild(card)
 }
 
 function getAllUsers(name) {
   fetch(`https://api.github.com/search/users?q=${name}`)
-  .then(res => res.json())
-  .then(data => data.items.forEach(person => renderPerson(person)))
+    .then(res => res.json())
+    .then(data => {
+      data.items.forEach(person => renderPerson(person))
+    })
+}
+
+function renderRepos(repo, index) {
+  const li = document.createElement('li') 
+  li.className = 'repo'
+  li.textContent = index + '. ' + repo.name
+  document.querySelector('#repos-list').appendChild(li)
+}
+
+function clearPreviousRepos() {
+  const repos = document.querySelector('#repos-list')
+  repos.innerText = ''
+}
+
+function addTitle(name) {
+  const h2 = document.createElement('h2')
+  h2.textContent = `${name}'s Repos`
+  document.querySelector('#repos-list').appendChild(h2)
 }
